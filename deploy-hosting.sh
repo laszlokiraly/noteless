1d9922641ef558f231ea9222f7044667c089144a
László Király <laszlo.t.kiraly@gmail.com>
2017-05-06 20:01:48 +0200

    automated setting of gzip and cache headers

--- deploy-hosting.sh
+++ deploy-hosting.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 npm run build
 find ./build/static/js -type f -exec gzip -9 "{}" \; -exec mv "{}.gz" "{}" \;
-aws s3 cp build s3://$NOTELESS_BUCKET/ --recursive
-open -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome https://console.aws.amazon.com/s3/buckets/$NOTELESS_BUCKET/static/js/\?region\=us-east-1\&tab\=overview
-echo "Cache-Control: public, max-age=31536000"
-echo "Content-Encoding: gzip"
+aws s3 cp ./build s3://$NOTELESS_BUCKET/ --recursive
+find ./build/static/js -type f -exec touch '{}' \;; aws s3 sync ./build/static/js s3://$NOTELESS_BUCKET/static/js --cache-control public,max-age=604800 --content-encoding gzip
